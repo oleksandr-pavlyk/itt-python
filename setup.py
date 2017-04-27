9e76d96fcbf09cf6eaedb66cdc6b48e5e5034f36
R. C. Thomas <rcthomas@lbl.gov>
2017-04-27 16:18:09 -0700

    Clean up

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ from distutils.command.build_ext import build_ext as _build_ext
 import os
 
 class build_ext(_build_ext):
+
     _build_ext.user_options += [
         ('vtune=', None, 'specify VTune installation directory'),
     ]
+
     def initialize_options(self):
         _build_ext.initialize_options(self) 
         self.vtune = os.environ.get("VTUNE_AMPLIFIER_XE_2017_DIR")
@@ -22,8 +24,6 @@ class build_ext(_build_ext):
 
 extension = Extension("itt", 
         sources = ["itt/itt-python.c"])
-#       extra_compile_args = ['-I/opt/intel/vtune_amplifier_xe_2017.2.0.499904/include'],
-#       extra_objects = ["/opt/intel/vtune_amplifier_xe_2017.2.0.499904/lib64/libittnotify.a"])
 
 setup(name = 'itt',
         version = '0.0.1',

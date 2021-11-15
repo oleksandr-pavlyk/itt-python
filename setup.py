f88ccd06730256746e0b5681f55f7aabfb0c48b0
Artem Kashkanov <artem.kashkanov@intel.com>
2021-11-15 10:38:49 +0300

    add itt pt api

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
 from distutils.core import setup, Extension
 from distutils.command.build_ext import build_ext as _build_ext
+from distutils.command.install import install as _install
 
 import os
+
+class InstallCommand(_install):
+    user_options = _install.user_options + [
+        ('vtune=', None, 'specify VTune installation directory'),
+        ]
+
+    def initialize_options(self):
+        _install.initialize_options(self)
+        self.vtune = None
+
+    def finalize_options(self):
+        _install.finalize_options(self)
+
+    def run(self):
+        global vtune
+        vtune = self.vtune
+        _install.run(self)  # OR: install.do_egg_install(self)
+
 
 class build_ext(_build_ext):
 
@@ -15,6 +34,8 @@ class build_ext(_build_ext):
 
     def finalize_options(self):
         _build_ext.finalize_options(self) 
+        if vtune is not None:
+            self.vtune = vtune
         assert self.vtune is not None, "Undefined VTune installation directory"
         assert os.path.isdir(self.vtune), "VTune installation path not a directory"
         self.include_dirs.append(os.path.join(self.vtune, "include"))
@@ -26,7 +47,7 @@ extension = Extension("itt",
         sources = ["itt/itt-python.c"])
 
 setup(name = 'itt',
-        version = '0.0.3',
+        version = '0.0.4',
         description = 'ITT API bindings for Python',
         ext_modules = [extension],
-        cmdclass={'build_ext': build_ext})
+        cmdclass={'build_ext': build_ext,  'install': InstallCommand})
